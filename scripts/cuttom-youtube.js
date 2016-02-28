09a774a9ea6a5550ecede313cd996ec633812d5f
quineghost <quineghost@gmail.com>
2016-02-28 14:54:27 +0900

    deleted cuttom-youtube.js

--- scripts/cuttom-youtube.js
+++
@@ -1,7 +0,0 @@
-//YouTube
-var currentTabUrl = location.href
-
-if(currentTabUrl.indexOf("youtube") != -1) {
-	var header = document.querySelector("#yt-masthead-container");
-	// header.style.opacity = 0.5;
-}
